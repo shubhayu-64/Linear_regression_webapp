833a712a31c6bc3aa62dbbb6d5fe5624eafe2b56
shubhayu-64 <majumdarshubhayu@gmail.com>
2021-10-03 11:57:03 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,235 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import imageio
+import os
+import shutil
+
+
+def generate(slope, intercept):
+    data_points, randomness = 200, 30
+    x_data = np.linspace(0, 400, data_points)
+    rand = np.random.randint(-randomness, randomness, size=data_points)
+    # slope, intercept = 0.789, 60
+    y_data = slope * x_data + intercept
+    y_data = y_data + rand
+    dataset = np.column_stack([x_data, y_data])
+    df = pd.DataFrame(dataset, columns=["x", "y"])
+    df.to_csv("dataset.csv")
+
+
+def gif(df, x, y, m, c):
+    os.makedirs("gif")
+    filenames = []
+    prog = 0.0
+    my_bar = st.progress(prog)
+    for i in range(0, len(m), 20):
+        plt.scatter(df[x], df[y], c="blue")
+        plt.plot(df[x], (m[i]*df[x]+c[i]))
+        plt.title("Regression Plot")
+        plt.xlabel("x")
+        plt.ylabel("y")
+
+        filename = f'./gif/{i}.png'
+        filenames.append(filename)
+
+        plt.savefig(filename)
+        plt.close()
+
+        my_bar.progress(prog)
+        prog = prog + (1.0/len(m))*20
+
+    with imageio.get_writer('plot.gif', mode='I') as writer:
+        for filename in filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+    shutil.rmtree("gif")
+
+
+def regression(df, epochs_limit, learning_rate, x, y, intercept_priority):
+    m = 0
+    c = 0
+    m_ar = []
+    c_ar = []
+
+    losses = []
+    data_len = len(df)
+    loss = 100000000
+    loss_dif = epochs_limit + 1
+    epoch_count = 0
+    log = st.empty()
+    while(loss_dif > epochs_limit):
+        sum_m = 0
+        sum_c = 0
+        dm = 0
+        dc = 0
+        prev_loss = loss
+        for d in range(data_len):
+            sum_m = sum_m + (df[x][d] * (df[y][d] - (m*df[x][d]+c)))
+            sum_c = sum_c + (df[y][d] - (m*df[x][d]+c))
+            loss = loss + ((df[y][d] - (m*df[x][d]+c))
+                           * (df[y][d] - (m*df[x][d]+c)))
+        dm = (-2/data_len)*sum_m
+        dc = (-2/data_len)*sum_c * intercept_priority
+        loss = loss/data_len
+
+        m = m-learning_rate*dm
+        c = c-learning_rate*dc
+        losses.append(loss)
+
+        m_ar.append(m)
+        c_ar.append(c)
+        loss_dif = prev_loss - loss
+
+        log.empty()
+        log.metric(label="Current Loss", value=loss, delta=-loss_dif)
+        epoch_count = epoch_count+1
+
+    st.write("Developing GIF, hold on... ")
+    gif(df, x, y, m_ar, c_ar)
+
+    return losses, m, c, epoch_count
+
+
+def run(epochs_limit, intercept_priority):
+    x = "x"
+    y = "y"
+
+    st.header("Dataset and Scatter Plot     ")
+    df = pd.read_csv("dataset.csv", usecols=[x, y])
+
+    col1, col2 = st.columns([1, 2])
+    col1.dataframe(df)
+
+    fig1 = plt.figure(1)
+    plt.scatter(df[x], df[y])
+    plt.title("Scatter Plot")
+    col2.pyplot(fig1)
+
+    st.write("***\n")
+
+    st.header("Running Regression ")
+    losses = []
+    losses, m, c, epochs = regression(
+        df, epochs_limit, 0.0000001, x, y, intercept_priority)
+    st.write("***")
+
+    st.header("Predictions ")
+    d1, d2 = st.columns(2)
+    d1.metric(label="Slope ", value=m)
+    d2. metric(label="Intecept ", value=c)
+
+    st.write("***")
+    col1, col2 = st.columns(2)
+
+    fig2 = plt.figure(2)
+    plt.scatter(df[x], df[y])
+    plt.plot(df[x], (m*df[x] + c))
+    plt.title("Predicted Regression Line")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    col1.pyplot(fig2)
+
+    fig3 = plt.figure(3)
+    plt.plot(list(range(epochs)), losses)
+    plt.title("Learning Curve")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    col2.pyplot(fig3)
+
+    st.header("Regression Line Animation")
+    st.image("plot.gif")
+
+
+def prep_dataset(dataset):
+    df = pd.read_csv(dataset)
+    x = str(st.sidebar.selectbox("Select dependent data", df.columns))
+    y = str(st.sidebar.selectbox("Select independent data", df.columns))
+
+    if st.sidebar.button("Confirm"):
+        x_data = df[x]
+        y_data = df[y]
+        dataset = np.column_stack([x_data, y_data])
+        df = pd.DataFrame(dataset, columns=["x", "y"])
+        df.to_csv("dataset.csv")
+        st.balloons()
+
+
+def main():
+    # ----------------------------- Formatting --------------------------------
+    st.set_page_config(page_title="Linear Regression",
+                       page_icon="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/259/mage_1f9d9.png")
+    hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden; }
+    footer:after {content:'Developed by Shubhayu Majumdar'; visibility: visible;}
+    </style>
+    """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.image("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/259/mage_1f9d9.png", width=80)
+
+    st.title("Linear Regression ")
+    st.write("""
+    [![Follow](https://img.shields.io/twitter/follow/shubhayu64?style=social)](https://twitter.com/shubhayu64)
+    &nbsp[![Github](https://img.shields.io/github/followers/shubhayu-64?style=social)](https://github.com/shubhayu-64)
+    &nbsp[![Buy me a coffee](https://img.shields.io/badge/Buy%20me%20a%20coffee--yellow.svg?logo=buy-me-a-coffee&logoColor=orange&style=social)](https://www.buymeacoffee.com/shubhayu64)
+
+    Linear regression in Python with a generated dataset.
+    The dataset contains 200 data distributed evenly. Vary the slope and intercept for generated data to play around.
+
+    This model uses __Adaptive Epochs__ over __Gradient Descent Algorithm__ based on the learning rate limit. 
+
+    Enter your choices on the sidebar. 
+
+    ##### PS. It may get boring at times but it works.
+    ***
+    """)
+
+    if st.sidebar.selectbox("Choose type of dataset", ["Generate", "Upload"]) == "Generate":
+        st.sidebar.write(
+            """
+            Generate a new dataset here. 
+            Enter Slope and Intercept to generate random data.
+            """)
+        if st.sidebar.selectbox("Generate new dataset", ["Yes", "No"]) == "Yes":
+            slope = float(st.sidebar.text_input("Enter slope ", 0.528))
+            intercept = float(st.sidebar.text_input("Enter intercept ", 60))
+            if st.sidebar.button("Generate "):
+                generate(slope, intercept)
+                st.balloons()
+        st.sidebar.markdown("<br>", unsafe_allow_html=True)
+        intercept_priority = float(st.sidebar.text_input(
+            "Enter Intercept Priority parameter ", 1000))
+
+        st.sidebar.write("***")
+        epochs_limit = float(st.sidebar.text_input(
+            "Enter Learning Rate Limit ", 0.001))
+
+        if st.sidebar.button("Start Regression"):
+            run(epochs_limit, intercept_priority)
+
+    else:
+        with st.sidebar.subheader('Upload your dataset'):
+            dataset = st.sidebar.file_uploader(
+                "Please upload a file of type: csv", type=["csv"])
+        if dataset is not None:
+            prep_dataset(dataset)
+
+        st.sidebar.markdown("<br>", unsafe_allow_html=True)
+        intercept_priority = float(st.sidebar.text_input(
+            "Enter Intercept Priority parameter ", 1000))
+
+        st.sidebar.write("***")
+        epochs_limit = float(st.sidebar.text_input(
+            "Enter Learning Rate Limit ", 0.001))
+
+        if st.sidebar.button("Start Regression"):
+            run(epochs_limit, intercept_priority)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,100 @@
+from math import sqrt
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+def main():
+    p = 1.5
+    q = -10
+
+    x = []
+    y = []
+
+    for a in range(0, 100, 5):
+        x.append(a)
+
+    for a in range(0, 100, 5):
+        # y = (mx + c) + random value
+        # The predicted line should have m close to p, c close to q
+        y.append(p*a + q + float(random.randint(-100, 100)/10))
+
+    # --------------------------------------------------------------------------
+    # Predicted Line
+    # y = mx + c
+    m = 0
+    c = 0
+
+    learning_rate = 0.0000005
+    epochs = 1000
+    loss_value = epochs
+    prev_loss = 0
+    loss_diff = 1
+
+    loss = []
+    n_epoch = []
+
+    # for e in range(epochs):
+    e = 0
+    while(loss_diff > 0.00001):
+        dm = 0
+        dc = 0
+        sum1 = 0
+        sum2 = 0
+        err = 0
+        prev_loss = loss_value
+        e = e+1
+
+        for k in range(len(x)):
+            sum1 = sum1 + (x[k]*(y[k]-(m*x[k]+c)))
+            sum2 = sum2 + (y[k]-(m*x[k]+c))
+            err = err + ((y[k]-(m*x[k]+c))*(y[k]-(m*x[k]+c)))
+
+        dm = (-2/len(x))*sum1
+        dc = (-2/len(x))*sum2
+        loss_value = err/len(x)
+        m = m-learning_rate*dm
+        c = c-learning_rate*dc
+
+        n_epoch.append(e+1)
+        loss.append(loss_value)
+        loss_value = sqrt(loss_value)
+
+        loss_diff = prev_loss - loss_value
+        print(f"Epoch: {e}, loss: {loss_value}")
+
+    print(f"m= {m}, c= {c}")
+
+    y_preds = []
+    for a in range(len(x)):
+        y_preds.append(float(m*x[a] + c))
+
+    # print(len(x), len(y_preds))
+    plt.figure(1)
+    plt.plot(n_epoch, loss, c="blue")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.title("Loss Curve")
+    plt.grid(True)
+
+    plt.figure(2)
+    plt.plot(x, y_preds, c="red")
+    plt.scatter(x, y, c="blue")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("Linear Regression")
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+    # x = [1, 2, 3, 4, 5]
+    # y = [2, 4, 6, 8, 10]
+    # plt.plot(x, y)
+
+    # plt.xlabel("X-Axis")
+    # plt.ylabel("Y-Axis")
+    # plt.title("Linear Regression")
+    # plt.grid(True)
+    # plt.show()
